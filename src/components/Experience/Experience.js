a3a8cd3985d10f1151b6a5781d0c0ec0f32a42a2
Harikrishna Mekala <hkmekala@outlook.com>
2023-04-26 23:37:41 +0530

    ftr: Added TechStack to job description.

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -2,18 +2,22 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { List, ListItem } from '@mui/material';
+import { List, ListItem, Button, Stack } from '@mui/material';
 
 
 
 
 function Experience(payload) {
-    const {type, time, role, description, companyName } = payload.job;
+    const {type, time, role, description, companyName, tech } = payload.job;
     const descriptionComponents = description.map((element) => (
       <ListItem key={element} variant="body2" sx={{fontFamily: 'monospace', fontWeight: 'light', lineHeight:1, display:'list-item' }}>
       {element}
       <br />
     </ListItem>
+    ));
+
+    const techStack = tech.map((element) => (
+      <Button variant="outlined" size="small" key={element} disableElevation>{element}</Button>
     ));
 
     return (
@@ -28,6 +32,9 @@ function Experience(payload) {
           <Typography sx={{ mb: 1.5, fontFamily: 'monospace' }} color="text.secondary">
           {role}
           </Typography>
+          <Stack spacing={1} direction="row">
+          {techStack}
+          </Stack>  
           <List>
           {descriptionComponents}
           </List>
